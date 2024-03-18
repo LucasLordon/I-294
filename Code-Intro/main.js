0fa8ffeb06699e1deaef231e221615aa4ee38f6e
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-03-18 11:20:07 +0100

    intro vue [5]

--- Code-Intro/main.js
+++ Code-Intro/main.js
@@ -9,7 +9,10 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             onSale:true,
             inStock:false,
-            inventory:100
+            inventory:100,
+            //mountedApp.details.push('30% wool');
+            details: ['50% cotton','50% cotton', '30% wool', '20% polyester'],
+            variants: [{id:2234,color:'green'},{id: 2235,color:'blue'}]
         }
     }
 })
