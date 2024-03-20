83195b4a2775cd7d41c32b407296a35f151e2f15
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-03-20 15:16:14 +0100

    to step 8

--- Code-Intro/main.js
+++ Code-Intro/main.js
@@ -1,18 +1,52 @@
 const app = Vue.createApp({
     data(){
         return{
+            cart:0,
             //Editer depuis la console : mountedApp.product = 'Shoes'
             product:'Brick',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             //Editer depuis la console : mountedApp.description = 'new description'
             description:'my description',
             //Editer depuis la console : mountedApp.image = './assets/images/socks_green.jpg'
             image: './assets/images/socks_blue.jpg',
             onSale:true,
-            inStock:false,
             inventory:100,
             //mountedApp.details.push('30% wool');
-            details: ['50% cotton','50% cotton', '30% wool', '20% polyester'],
-            variants: [{id:2234,color:'green'},{id: 2235,color:'blue'}]
+            details: ['50% cotton','50% cotton', '30% wool'],
+            variants: [
+                {id:2234,color:'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                {id: 2235,color:'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+            ],
+            activeClass: true
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        removeToCart(){
+            this.cart -=1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
         }
     }
 })
